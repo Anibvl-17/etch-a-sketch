80ceaff7936f1a033ba72f19e0242b5b0e548be1
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2024-12-24 19:44:55 -0300

    Add 'hover' event to grid-divs

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ for (let i = 0; i < 16; i++) {;
     for (let j = 0; j < 16; j++) {
         const div = document.createElement("div");
         div.className = "grid-div";
+        div.addEventListener("mouseover", function() {
+            div.classList.add("hovered");
+        });
+
         container.appendChild(div);
     }
 }
